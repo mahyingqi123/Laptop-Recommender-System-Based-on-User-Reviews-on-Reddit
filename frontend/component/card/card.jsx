93b8578bfb85eb226cee8cc562dbaf223634ca47
NgJunQuan <jngg0106@student.monash.edu>
2024-05-14 16:39:41 +0800

    edit card element

--- frontend/component/card/card.jsx
+++ frontend/component/card/card.jsx
@@ -1,18 +1,19 @@
-import { FaStar } from "react-icons/fa";
-import {useRouter} from 'next/navigation'
+"use client";
+import { useState } from "react";
+import Modal from "@component/modal/modal";
 import "./card.scss";
+import { Button } from "@headlessui/react";
+import { BiSolidCommentDetail, BiSolidUpvote } from "react-icons/bi";
 
 const Card = ({ laptop }) => {
-    const router = useRouter();
-
-    const handleDetail = () => {
-        router.push(`/product-page?id=${laptop.id}`)
-      }
+    const [openModal, setOpenModal] = useState(false);
+    const defaultImage = "/images/No_Image_Available.jpg";
 
     return (
+    <div>
         <div className="laptop-list">
             <div key={laptop.id} className="laptop-card">
-                <img src={laptop.image} className="laptops-image" />
+                <img src={laptop.image || defaultImage} className="laptops-image"/>
                 <div className="laptop-content">
                     <div className="displayStack1">
                         <h3 className="laptopName">{laptop.name}</h3>
@@ -20,17 +21,25 @@ const Card = ({ laptop }) => {
                     </div> 
                     <div className="displayStack2">
                         <div className="productRating">
-                            {[...Array(laptop.rating)].map((index) => (
-                                <FaStar id={index+1} key = {index}/>
-                            ))}
+                            <div className="rating"> {Math.round(laptop.score)}% </div>
+                        </div>
+                        <div className="interaction">
+                            <BiSolidCommentDetail/>
+                            <span>{laptop.total_comments}</span> {/* Replace with your actual data */}
+                        </div>
+                        <div className="interaction">
+                            <BiSolidUpvote />
+                            <span>{laptop.total_upvotes}</span> {/* Replace with your actual data */}
                         </div>
                         <div className="productDetail">
-                            <a onClick={handleDetail}>View Details</a>
+                            <Button onClick={()=> setOpenModal(true)}>View Details</Button>
                         </div>
                     </div> 
                 </div>
             </div>
         </div>
+        <Modal open={openModal} onClose={()=> setOpenModal(false)} laptopID={laptop.id}/>
+    </div>
     );
 }
 
