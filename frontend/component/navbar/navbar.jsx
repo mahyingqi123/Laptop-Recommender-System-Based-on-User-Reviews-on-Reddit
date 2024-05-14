9477d2dabbba41bc68801b01fb148289c03d766f
NgJunQuan <jngg0106@student.monash.edu>
2024-05-14 16:48:58 +0800

    add about us page and remove product page

--- frontend/component/navbar/navbar.jsx
+++ frontend/component/navbar/navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <Link href="#">Logo</Link>
       <ul>
         <li className="/"><Link href="/">Home</Link></li>
-        <li><Link href="/result-page">About Us</Link></li>
+        <li><Link href="/about-page">About Us</Link></li>
       </ul>
     </nav>
   )
